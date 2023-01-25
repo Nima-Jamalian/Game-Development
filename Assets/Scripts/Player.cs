a942f3c5f59e466ed60bfea805af1a03c5b4ff5c
Nima Jamalian <nima.jamalian80@gmail.com>
2023-01-25 10:20:35 +0000

    FPS Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float horizontalInput;
+    float verticalInput;
+    float mouseX;
+    float mouseY;
+    Vector3 direction;
+    Vector3 velocity;
+    CharacterController characterController;
+    float speed = 1f;
+    float gravity = 9.8f;
+    [SerializeField] float mouseSensivity = 1f;
+    [SerializeField] float walkingSpeed = 1f;
+    [SerializeField] float runningSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Debug.Log(transform.position);
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterMovement();
+        MouseMovement();
+    }
+
+    void CharacterMovement() {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        direction = new Vector3(horizontalInput, 0, verticalInput);
+        velocity = direction * speed;
+        velocity.y -= gravity; //velocity.y = velocity.y - gravity
+        velocity = transform.TransformDirection(velocity);
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runningSpeed;
+        }
+        else
+        {
+            speed = walkingSpeed;
+        }
+    }
+
+    void MouseMovement() {
+        mouseX = Input.GetAxis("Mouse X");
+        Vector3 newRotationX = transform.localEulerAngles;
+        newRotationX.y += mouseX * mouseSensivity;
+        transform.localEulerAngles = newRotationX;
+
+
+        mouseY = Input.GetAxis("Mouse Y");
+        Vector3 newRotationY = Camera.main.transform.localEulerAngles;
+        newRotationY.x -= mouseY * mouseSensivity;
+        Camera.main.transform.localEulerAngles = newRotationY;
+    }
+
+
+}
